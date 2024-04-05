d4533f8cc1f6826f095ef6d1196e16cde961b836
GopiNavadiya <divyeshpaneliyaflutter.artonest@gmail.com>
2024-04-05 17:10:06 +0530

    Work on Main Screen UI.

--- lib/UI/Screens/ProfileSection/profile_binding.dart
+++ lib/UI/Screens/ProfileSection/profile_binding.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:kayle/UI/Screens/CartSection/cart_controller.dart';
 import 'package:kayle/UI/Screens/ProfileSection/profile_controller.dart';
 
 class ProfileBinding extends Bindings {
